b6f1d188d264b62003237de5f38f661658ab71e6
alvaro-dda <alvarodda12@gmail.com>
2023-12-03 17:27:26 +0100

    numeric_features build

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 estados = [
     "estado_fenologico_1",
@@ -21,17 +22,16 @@ def load_dataset():
     return df
 
 
-def find_estado_with_value_two(row) -> int:
-    """
-    Returns numero del siguiente etado (yt+1). (e.g., 13)
-    """
-    for column in row.index:
-        if row[column] == 2:
-            number_growth_stage = int(column.split("_")[-1])
-            return number_growth_stage
-
-
 def get_valid_dataset(df: pd.DataFrame(), max_days_till_next_date: int) -> pd.DataFrame():
+    
+    def find_estado_with_value_two(row) -> int:
+        """
+        Returns numero del siguiente etado (yt+1). (e.g., 13)
+        """
+        for column in row.index:
+            if row[column] == 2:
+                number_growth_stage = int(column.split("_")[-1])
+                return number_growth_stage
     
     # Removing null and filling empty estados with 0
     df = df.dropna(subset=estados, how="all")
@@ -86,19 +86,62 @@ def build_spine() -> pd.DataFrame():
     return df['codparcela','fecha','next_estado']
 
 
-def build_numeric_features_parcela(df: pd.DataFrame) -> pd.DataFrame():
+def build_numeric_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
+
+    """
+    Returns a dataset with the numeric features for each field.
+    """
+    # We are not using '305_diámetro_de_copa_(m)', '311_fecha_de_plantación_variedad_principal', '306_altura_de_copa_(m)'
+    # As they have > 70% NA
+    numeric_features = ['104_altitud_(m)',
+    '201_superf_cultivada_en_la_parcela_agrícola_(ha)',
+    '202_pendiente_(%)',
+    '301_marco_(m_x_m)',
+    '302_densidad_(plantas/ha)',
+    '303_nº_pies_por_árbol',
+    'porcentaje_floracion']
+
+    df = load_dataset()
+    df = get_valid_dataset(df,32)
+    df_parcelas = df.groupby(by='codparcela')[numeric_features].first() #Selects first non-empty entry for each feature
+
+    def replace_nullwithmean_remove_outliers(df, threshold = np.inf):
+        df = df.copy()
+        mean_value = df.mean()
+        df.fillna(mean_value, inplace=True)
+        df.loc[df > threshold] = mean_value
+        return df
+    
+    #Replace outliers and null with mean
+    df_parcelas['104_altitud_(m)'] = replace_nullwithmean_remove_outliers(df_parcelas['104_altitud_(m)'], 2500)
+    df_parcelas['201_superf_cultivada_en_la_parcela_agrícola_(ha)'] = replace_nullwithmean_remove_outliers(df_parcelas['201_superf_cultivada_en_la_parcela_agrícola_(ha)'],10000)
+    df_parcelas['202_pendiente_(%)'] = replace_nullwithmean_remove_outliers(df_parcelas['202_pendiente_(%)'],100)
+    df_parcelas['302_densidad_(plantas/ha)'] = replace_nullwithmean_remove_outliers(df_parcelas['302_densidad_(plantas/ha)'],10000)
+    df_parcelas['303_nº_pies_por_árbol'] = replace_nullwithmean_remove_outliers(df_parcelas['303_nº_pies_por_árbol'],100)
+    df_parcelas['porcentaje_floracion'] = replace_nullwithmean_remove_outliers(df_parcelas['porcentaje_floracion'],100)
+
+    df_parcelas['301_marco_(m_x_m)'] = df_parcelas['301_marco_(m_x_m)'].astype(str).str.replace(",", ".")
+    df_parcelas['301_marco_(m_x_m)'] = df_parcelas['301_marco_(m_x_m)'].str.extract(r'(\d+\.\d+|\d+)')[0].astype(float) #Finds the first number (e.g. from 1.5 x 1.5, finds 1.5)
+    df_parcelas['301_marco_(m_x_m)'] = replace_nullwithmean_remove_outliers(df_parcelas['301_marco_(m_x_m)'],100)
+
+    #Datatypes
+    df_parcelas['104_altitud_(m)'].astype('int32', inplace=True)
+    df_parcelas['201_superf_cultivada_en_la_parcela_agrícola_(ha)'].astype('float32', inplace=True)
+    df_parcelas['202_pendiente_(%)'].astype('int32', inplace=True)
+    df_parcelas['302_densidad_(plantas/ha)'].astype('int32', inplace=True)
+    df_parcelas['303_nº_pies_por_árbol'].astype('int32', inplace=True)
+    df_parcelas['porcentaje_floracion'].astype('int32', inplace=True)
+    df_parcelas['301_marco_(m_x_m)'].astype('float32', inplace=True)
+
+    return pd.merge(spine, df_parcelas, left_on='codparcela',right_index=True, how='left')
+
+def build_binary_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
     """
     Returns a dataset with the features for each field.
     """
     pass
 
-def build_binary_features_parcela() -> pd.DataFrame():
-    """
-    Returns a dataset with the features for each field.
-    """
-    pass
-
-def build_categorical_features_parcela() -> pd.DataFrame():
+def build_categorical_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
     """
     Returns a dataset with the features for each field.
     """
