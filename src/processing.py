c3c0698d179c28adbfc4ce2ffaa1c4a63d24f387
alvaro-dda <alvarodda12@gmail.com>
2023-12-05 21:51:12 +0100

    new changes

--- src/processing.py
+++ src/processing.py
@@ -18,7 +18,7 @@ estados = [
     "estado_fenologico_14"]
 
 def load_dataset():
-    df = pd.read_parquet("notebooks/subset_muestreos_parcelas.parquet")
+    df = pd.read_parquet("subset_muestreos_parcelas.parquet")
     df = get_valid_dataset(df,32)
     return df
 
@@ -54,6 +54,7 @@ def get_valid_dataset(df: pd.DataFrame(), max_days_till_next_date: int) -> pd.Da
     # Creating a column to display the next estado_fenológico (yt+1)
     df["estado_actual"] = df[estados].apply(find_estado_with_value_two, axis=1)
     df["next_estado"] = df.groupby("codparcela", observed=True)["estado_actual"].shift(-1)
+    df["next_y"] = df["next_estado"] - df["estado_actual"]
 
     # Removing the parcels with only one entry and the last entry for every parcel
     df = df.dropna(subset=["days_until_next_visit"])  # 5150 entries removed
@@ -70,7 +71,7 @@ def get_valid_dataset(df: pd.DataFrame(), max_days_till_next_date: int) -> pd.Da
     # Filtering the max days
     df = df[df["days_until_next_visit"] < max_days_till_next_date]
 
-    excluded_columns = ['count_2s', 'next_date']
+    excluded_columns = ['count_2s', 'next_date', 'next_estado']
 
     return df.loc[:, [i for i in df.columns if i not in excluded_columns]]
 
@@ -82,7 +83,7 @@ def build_spine() -> pd.DataFrame():
 
     df = load_dataset()
     
-    return df[['codparcela','fecha','next_estado']+ estados]
+    return df[['codparcela','fecha','next_y']]
 
 
 def build_numeric_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
@@ -123,13 +124,13 @@ def build_numeric_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
     df_parcelas['301_marco_(m_x_m)'] = replace_nullwithmean_remove_outliers(df_parcelas['301_marco_(m_x_m)'],100)
 
     #Datatypes
-    df_parcelas['104_altitud_(m)'].astype('int32', inplace=True)
-    df_parcelas['201_superf_cultivada_en_la_parcela_agrícola_(ha)'].astype('float32', inplace=True)
-    df_parcelas['202_pendiente_(%)'].astype('int32', inplace=True)
-    df_parcelas['302_densidad_(plantas/ha)'].astype('int32', inplace=True)
-    df_parcelas['303_nº_pies_por_árbol'].astype('int32', inplace=True)
-    df_parcelas['porcentaje_floracion'].astype('int32', inplace=True)
-    df_parcelas['301_marco_(m_x_m)'].astype('float32', inplace=True)
+    df_parcelas['104_altitud_(m)'] = df_parcelas['104_altitud_(m)'].astype('int32')
+    df_parcelas['104_altitud_(m)'] = df_parcelas['104_altitud_(m)'].astype('float32')
+    df_parcelas['202_pendiente_(%)'] = df_parcelas['202_pendiente_(%)'].astype('int32')
+    df_parcelas['302_densidad_(plantas/ha)'] = df_parcelas['302_densidad_(plantas/ha)'].astype('int32')
+    df_parcelas['303_nº_pies_por_árbol'] = df_parcelas['303_nº_pies_por_árbol'].astype('int32')
+    df_parcelas['porcentaje_floracion'] = df_parcelas['porcentaje_floracion'].astype('int32')
+    df_parcelas['301_marco_(m_x_m)'] = df_parcelas['301_marco_(m_x_m)'].astype('float32')
 
     return pd.merge(spine, df_parcelas, left_on='codparcela',right_index=True, how='left')
 
@@ -152,8 +153,7 @@ def build_binary_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
 
     #For now replacing null with 0 and the rest to a binay 1 or 0
     replace_dict = {'Si': 1, 'No': 0, 'NO': 0,'SI':1}
-    df_parcelas["211_utilización_de_cubierta_vegetal"] = df_parcelas["211_utilización_de_cubierta_vegetal"].replace(replace_dict).fillna(0)
-    df_parcelas["211_utilización_de_cubierta_vegetal"].astype(int, inplace=True)
+    df_parcelas["211_utilización_de_cubierta_vegetal"] = df_parcelas["211_utilización_de_cubierta_vegetal"].replace(replace_dict).fillna(0).astype(int)
 
     replace_dict_riego = {'Pozo': 1, 'POZO': 1}
 
@@ -166,8 +166,7 @@ def build_binary_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
         else:
             return 0
 
-    df_parcelas['208_riego:_procedencia_del_agua'] = df_parcelas['208_riego:_procedencia_del_agua'].astype('str').apply(apply_dict, replace_dict=replace_dict_riego).fillna(0)
-    df_parcelas['208_riego:_procedencia_del_agua'].astype(int, inplace=True)
+    df_parcelas['208_riego:_procedencia_del_agua'] = df_parcelas['208_riego:_procedencia_del_agua'].astype('str').apply(apply_dict, replace_dict=replace_dict_riego).fillna(0).astype(int)
 
     dict_replace_rieg= {}
     for i in df_parcelas['207_riego:_sistema_usual_de_riego'].str.lower().unique():
@@ -177,14 +176,20 @@ def build_binary_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
             dict_replace_rieg[i] = 1
         else: dict_replace_rieg[i] = 0
     
-    df_parcelas['207_riego:_sistema_usual_de_riego'] = df_parcelas['207_riego:_sistema_usual_de_riego'].apply(apply_dict, replace_dict=dict_replace_rieg).fillna(0)
-    df_parcelas['207_riego:_sistema_usual_de_riego'].astype(int, inplace=True)
-
+    df_parcelas['207_riego:_sistema_usual_de_riego'] = df_parcelas['207_riego:_sistema_usual_de_riego'].apply(apply_dict, replace_dict=dict_replace_rieg).fillna(0).astype(int)
     replace_dict_gest = {'Producción Integrada (PI)': 1}
-    df_parcelas['109_sistema_para_el_cumplimiento_gestión_integrada'] = df_parcelas['109_sistema_para_el_cumplimiento_gestión_integrada'].apply(apply_dict, replace_dict=replace_dict_gest).fillna(0)
-    df_parcelas['109_sistema_para_el_cumplimiento_gestión_integrada'].astype(int, inplace=True)
+    df_parcelas['109_sistema_para_el_cumplimiento_gestión_integrada'] = df_parcelas['109_sistema_para_el_cumplimiento_gestión_integrada'].apply(apply_dict, replace_dict=replace_dict_gest).fillna(0).astype(int)
 
     return pd.merge(spine, df_parcelas, left_on='codparcela',right_index=True, how='left')
+
+
+def build_date_variables_parcelas(spine: pd.DataFrame) -> pd.DataFrame():
+
+    df = load_dataset()
+    relevant_cols = estados + ['porcentaje_floracion','codparcela','fecha']
+    df = df[relevant_cols]
+
+    return pd.merge(spine, df, on=['codparcela','fecha'], how='left')
 
 
 def build_categorical_features_parcela(spine: pd.DataFrame) -> pd.DataFrame():
